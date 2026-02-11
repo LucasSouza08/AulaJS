1148ea810f30362c6ed95679060346ca4c560119
LucasSouza08 <lucas.marcelino0403@gmail.com>
2026-02-11 15:05:14 -0300

    11/02/26

---
+++ Aula01/script.js
@@ -0,0 +1,18 @@
+var nome = "Lucas";
+
+var idade = 16;
+
+var altura = 1.81;
+
+console.log( nome );
+console.log( idade );
+console.log( altura );
+
+document.write("<h1>Olá, seja bem-vindo ao JavaScript</h1>")
+
+document.write( nome + "<br>");
+document.write( idade + "<br>");
+document.write( altura + "<br>" );
+
+var dias = idade * 365
+document.write("<p style='color: red'> "Você ja viveu " + dias + "dias" < /p>")
